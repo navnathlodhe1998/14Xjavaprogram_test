931b391d6a57a4d5bf6a59f63feb12407a9035f8
Navnath Londhe <navnathlondhe1998@gmail.com>
2025-09-20 15:00:34 +0530

    1st java program

---
+++ src/Test/Boolean.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Boolean {
+    public static void main(String[] args) {
+
+        boolean is_navnath_from_MH = true;
+        boolean isyouaremarried=false;
+
+
+    }
+}
