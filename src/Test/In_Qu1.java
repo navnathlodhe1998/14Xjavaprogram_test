947da9902c4cf9620b9984bd7ad56463d988110f
Navnath Londhe <navnathlondhe1998@gmail.com>
2025-09-20 14:52:09 +0530

    1st java program

---
+++ src/Test/In_Qu1.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class In_Qu1 {
+    public static void main(String[] args) {
+        int _2=123;
+
+        System.out.println(_2);
+    }
+}
---
+++ src/Test/In_Qu2.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class In_Qu2 {
+
+    public static void main(String[] args) {
+
+              //int enum = 10;
+        int Enum = 10;
+//        System.out.println(enum);
+               //int pramod dutta  = 89;
+        int pramod$dutta = 90;
+        int Lab020_Interview_QnA2 = 123;
+    }
+}
