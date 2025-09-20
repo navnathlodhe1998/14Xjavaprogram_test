405e60829dd21277d607e583e7b484a139b51e97
Navnath Londhe <navnathlondhe1998@gmail.com>
2025-09-20 14:44:25 +0530

    1st java program

--- src/Test/Variable_2.java
+++ src/Test/Variable_2.java
@@ -1,4 +1,17 @@
 package Test;
 
 public class Variable_2 {
+
+    public static void main(String[] args) {
+
+        int age =25;
+
+        age=111;
+        age=40;
+
+        System.out.println(age);
+
+
+
+    }
 }
