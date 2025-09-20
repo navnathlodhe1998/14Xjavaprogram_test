6fbe72dbef1e6e8120b402aa75f0ef795a6c76cc
Navnath Londhe <navnathlondhe1998@gmail.com>
2025-09-20 15:03:38 +0530

    1st java program

---
+++ src/Test/Numeric_data_types.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class Numeric_data_types {
+    public static void main(String[] args) {
+
+
+        byte b = 10;
+        short s = 10;
+        int i  = 10;
+        char c = 'A';
+
+        long phone = 976543210L;
+        long phone1 = 976543210l;
+
+        float f = 3.14f;
+        float f2 = 3.14F;
+
+        double d = 3.12123423;
+
+    }
+}
